4f229aacc74087ef64ca80d646a70bbcab0d115a
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-05-01 17:52:38 +0530

    add ejs file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const db = require('./dataBaseConfig.js')
 const userRouter = require('./route/userRoute.js')
@@ -6,6 +7,10 @@ const userRouter = require('./route/userRoute.js')
 let app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
 let port = 3000
 let hostname = '127.0.0.1'
 
@@ -33,7 +38,7 @@ db.query(userTableQuery, (err, result)=>{
 
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html')
+    res.render('index')
 })
 
 app.use('/api', userRouter)
--- route/userRoute.js
+++ route/userRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controller/userController.js')
 
-router.post('/saveData' , userController.saveData)
+router.post('/' , userController.saveData)
 
 router.get('/getData' , userController.getData)
 
