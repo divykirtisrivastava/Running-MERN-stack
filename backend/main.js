6f7733319bde13ba470df5a34107d24b9d0c5565
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-05-17 18:02:07 +0530

    add admin

--- backend/main.js
+++ backend/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const productRoute = require('./route/product.route.js')
+const adminRoute = require('./route/admin.route.js')
 const db = require('./dataBaseConfig.js')
 dotenv.config({
     path:'./.env'
@@ -34,8 +35,23 @@ db.query(userTableQuery, (err, result)=>{
     }
 })
 
+let adminTableQuery = `
+CREATE TABLE IF NOT EXISTS admin (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NULL,
+    password VARCHAR(255) NULL,
+    PRIMARY KEY (id));
+`
+db.query(adminTableQuery, (err, result)=>{
+    if(err) throw err
+    else{
+        console.log("user table is craeted")
+    }
+})
+
 app.listen(process.env.PORT, hostname , ()=>{
     console.log(`😃server is running at http://${hostname}:${process.env.PORT}/api/`)
 })
 
 app.use('/api', productRoute)
+app.use('/api', adminRoute )
